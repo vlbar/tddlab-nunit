d9472c40b56451d7e917d72161e69b33ecf07e5e
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2020-12-09 23:36:42 +0300

    Add push and pop test

---
+++ TDDStackLib/MyStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDStackLib
+{
+    class MyStack
+    {
+        public int Size { get; private set; }
+
+        public int Length { get; private set; }
+
+        public MyStack(int length)
+        {
+            Length = length;
+        }
+
+        public void Push(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Pop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDDStackLib/MyStackTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDStackLib
+{
+    [TestFixture]
+    class MyStackTest
+    {
+        [Test]
+        public void PushPopTest()
+        {
+            MyStack stack = new MyStack(3);
+            stack.Push(2);
+            stack.Push(1);
+            stack.Push(8);
+
+            Assert.AreEqual(8, stack.Pop());
+            Assert.AreEqual(2, stack.Size);
+        }
+    }
+}
